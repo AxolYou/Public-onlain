a4457b52c1041dc3bf17ee9f90d293323f8b1dd9
AxolYou <91787368+AxolYou@users.noreply.github.com>
2022-03-07 17:03:59 +0300

    Add files via upload
    Добавил файлы

---
+++ sc.js
@@ -0,0 +1,8 @@
+let btn1 = document.querySelector('#btn1')
+btn1.counter = 0
+btn1.onclick = function(){
+    alert('Документ Not_vurys_%$#$!^$_$%^@$^ был установлен на ваше устройство')
+    this.counter++
+    
+}
+function changeBG(object){}
